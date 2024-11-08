46f541a9cb6803fb97c1b6e235afef8f1a76ab11
Kourosh Alasti <coding@kouroshalasti.com>
2024-11-08 09:46:50 -0800

    feat: add following capability

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,15 +1,20 @@
 import { Router } from 'express';
 import validateUser from '../middlewares/validateUser.js';
 import {
-  createNewFollow, getAllUsers, getUser, removeNewFollow,
+  createNewFollow,
+  getAllUsers,
+  getUser,
+  removeNewFollow,
+  getMyFollowing,
 } from '../controllers/user.js';
 
 const router = Router();
 
 router.get('/', validateUser, getAllUsers);
-router.get('/:username', validateUser, getUser);
+router.get('/following', validateUser, getMyFollowing);
 router.put('/follow/:username', validateUser, createNewFollow);
 router.put('/unfollow/:username', validateUser, removeNewFollow);
+router.get('/:username', validateUser, getUser);
 // router.delete("/:username");
 // router.delete("/");
 
