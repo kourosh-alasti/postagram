9c8c1591c06a34ffe39ff24c00e92de50ce68ad2
Kourosh Alasti <coding@kouroshalasti.com>
2024-11-08 03:36:40 -0800

    feat: database queries for user model

---
+++ server/src/queries/user.js
@@ -0,0 +1,78 @@
+import User from '../models/user.js';
+
+export const createUser = async ({ username, password }) => {
+  try {
+    const newUser = await User.create({ username, password });
+
+    return newUser;
+  } catch (error) {
+    console.error('Error occured while creating new user');
+    console.error(error);
+    throw new Error(error);
+  }
+};
+
+export const getUserByUsername = async (username) => {
+  try {
+    const fetchedUser = await User.find({ username });
+    return fetchedUser;
+  } catch (error) {
+    console.error('Error while getting user');
+    console.error(error);
+    throw Error(error);
+  }
+};
+
+export const getUsers = async (limitBy = 10, offsetBy = 0) => {
+  try {
+    const users = await User.find().limit(limitBy).skip(offsetBy);
+
+    return users;
+  } catch (error) {
+    console.error('Error while getting users');
+    console.error(error);
+    throw Error(error);
+  }
+};
+
+export const addFollow = async ({ username, toFollow }) => {
+  try {
+    const fetchedUser = await User.updateOne(
+      {
+        username,
+      },
+      { $push: { following: { username: toFollow } } },
+    );
+
+    return fetchedUser;
+  } catch (error) {
+    console.error(`Error while following ${toFollow}`);
+    console.error(error);
+    throw new Error(error);
+  }
+};
+
+export const removeFollow = async ({ username, toUnfollow }) => {
+  try {
+    const fetchedUser = await User.updateOne(
+      { username },
+      { $pull: { following: { username: toUnfollow } } },
+    );
+
+    return fetchedUser;
+  } catch (error) {
+    console.error(`Error while unfollowing ${toUnfollow}`);
+    console.error(error);
+    throw new Error(error);
+  }
+};
+
+export const deleteAllUsers = async () => {
+  try {
+    await User.deleteMany({});
+  } catch (error) {
+    console.error('Error Deleting Users');
+    console.error(error);
+    throw new Error(error);
+  }
+};
