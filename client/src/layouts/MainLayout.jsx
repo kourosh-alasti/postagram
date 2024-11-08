578c2007bf9aa2cb7fb283e20ed56b0c415321fc
Kourosh Alasti <coding@kouroshalasti.com>
2024-11-08 09:44:41 -0800

    chore: add protected routes

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,14 +1,41 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
 
 const MainLayout = () => {
+  const [auth, setAuth] = useState(true);
+
+  useEffect(() => {
+    const handleAuth = async () => {
+      const response = await fetch("http://localhost:8000/api/auth/token", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const body = await response.json();
+      console.log(body);
+      setAuth(body.isAuthenticated);
+    };
+
+    handleAuth();
+  }, []);
+
   return (
-    <div className="flex h-full w-full flex-col items-center gap-3 bg-slate-900 text-white">
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </div>
+    <>
+      {auth ? (
+        <div className="flex h-full w-full flex-col items-center gap-3 bg-slate-900 text-white">
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </div>
+      ) : (
+        <Navigate to="/auth/sign-in" />
+      )}
+    </>
   );
 };
 
