1f4259b737dfd5b3d1392afb19bd3eb95ac96692
Kourosh Alasti <coding@kouroshalasti.com>
2024-11-08 09:44:23 -0800

    refactor: auth pages
    refactor and implement auth

--- client/src/pages/auth/SignInPage.jsx
+++ client/src/pages/auth/SignInPage.jsx
@@ -1,29 +1,53 @@
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const SignIn = () => {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("Login");
-    if (username === "" || password === "")
-      setError("Need both username and password to login");
+    if (!username || !password) {
+      toast.error("Please fill out all fields before trying to login!");
+    }
+
+    const response = await fetch("http://localhost:8000/api/auth/sign-in", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (data.error) {
+      return toast.error(data.error.message);
+    }
+
+    toast.success(data.message);
+
+    navigate("/");
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="w-full max-w-sm rounded-lg bg-white p-6 shadow-lg">
-        <h2 className="mb-4 text-center text-2xl font-bold text-gray-800">
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="w-full max-w-sm rounded-lg border border-slate-400 bg-slate-800 p-6 shadow-lg">
+        <h2 className="mb-4 text-center text-2xl font-bold text-slate-400">
           Sign In
         </h2>
 
         <form onSubmit={handleSubmit} className="space-y-4">
           <div className="flex flex-col gap-1.5">
             <label
-              className="mb-1 block text-sm font-semibold text-gray-700"
+              className="mb-1 block text-sm font-semibold text-slate-400"
               htmlFor="username_input"
             >
               Username
@@ -34,13 +58,13 @@ const SignIn = () => {
               id="username_input"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full rounded px-3 py-1 text-slate-800 focus:outline-none"
             />
           </div>
 
           <div className="flex flex-col gap-1.5">
             <label
-              className="mb-1 block text-sm font-semibold text-gray-700"
+              className="mb-1 block text-sm font-semibold text-slate-400"
               htmlFor="password_input"
             >
               Password
@@ -51,18 +75,23 @@ const SignIn = () => {
               id="password_input"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full rounded px-3 py-1 text-slate-800 focus:outline-none"
             />
           </div>
-          {error && <p className="text-sm text-red-500">{error}</p>}
-
           <button
             type="submit"
-            className="mt-2 w-full rounded-lg bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500"
+            className="mt-2 w-full rounded-lg border border-slate-500 bg-indigo-500 px-4 py-2 font-bold text-white hover:bg-indigo-600 focus:outline-none"
           >
             Login
           </button>
         </form>
+
+        <p className="mt-4 text-sm text-slate-500">
+          Don&apos;t have an account?{" "}
+          <Link to="/auth/sign-up">
+            <span className="font-semibold text-slate-200">Sign Up</span>
+          </Link>
+        </p>
       </div>
     </div>
   );
--- client/src/pages/auth/SignUpPage.jsx
+++ client/src/pages/auth/SignUpPage.jsx
@@ -1,41 +1,55 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const SignUp = () => {
   const navigate = useNavigate();
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (password !== confirmPassword) {
-      setError("Passwords do not match");
-    } else {
-      setError("");
-
-      setUsername("");
-      setPassword("");
-      setConfirmPassword("");
-
-      navigate("/");
+    if (!password || !username) {
+      return toast.error(
+        "Please fill out all fields before trying to sign up!",
+      );
     }
+
+    const response = await fetch("http://localhost:8000/api/auth/sign-up", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (data.error) {
+      return toast.error(data.error.message);
+    }
+
+    toast.success(data.message);
+
+    navigate("/");
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-lg">
-        <h2 className="mb-6 text-center text-2xl font-bold text-gray-800">
-          Register
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="w-full max-w-md rounded-lg border border-slate-400 bg-slate-800 p-8 shadow-xl">
+        <h2 className="mb-6 text-center text-2xl font-bold text-slate-400">
+          Sign Up
         </h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div className="flex flex-col gap-1.5">
             <label
               htmlFor="username_input"
-              className="mb-1 block text-sm font-semibold text-gray-700"
+              className="mb-1 block text-sm font-semibold text-slate-400"
             >
               Username
             </label>
@@ -45,14 +59,14 @@ const SignUp = () => {
               id="username_input"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full rounded px-3 py-1 text-slate-800 focus:outline-none"
             />
           </div>
 
-          <div className="5 flex flex-col gap-1">
+          <div className="flex flex-col gap-1.5">
             <label
               htmlFor="password_input"
-              className="mb-1 block text-sm font-semibold text-gray-700"
+              className="mb-1 block text-sm font-semibold text-slate-400"
             >
               Password
             </label>
@@ -62,39 +76,23 @@ const SignUp = () => {
               id="password_input"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full rounded px-3 py-1 text-slate-800 focus:outline-none"
             />
           </div>
 
-          <div className="5 flex flex-col gap-1">
-            <label
-              htmlFor="confirm-password_input"
-              className="mb-1 block text-sm font-semibold text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="text"
-              name="confirm-password_input"
-              id="confirm-password_input"
-              value={confirm}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-
-          {error && <p className="text-sm text-red-500">{error}</p>}
           <button
             type="submit"
-            className="foucs:outline-none w-full rounded-lg bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500"
+            className="foucs:outline-none w-full rounded-lg border border-slate-500 bg-indigo-600 px-4 py-2 font-bold text-white hover:bg-indigo-700 focus:ring-2 focus:ring-blue-500"
           >
             Sign Up
           </button>
         </form>
 
-        <p className="mt-4 text-center text-sm text-gray-600">
+        <p className="mt-4 text-sm text-slate-500">
           Already have an account?{" "}
-          <span onClick={() => navigate("/auth/sign-in")}> Login</span>
+          <Link to="/auth/sign-in">
+            <span className="font-semibold text-slate-200">Sign In</span>
+          </Link>
         </p>
       </div>
     </div>
