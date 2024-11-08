88bd5697dc5bf13bc9bc124a06ebde711b1abc06
Kourosh Alasti <coding@kouroshalasti.com>
2024-11-08 09:47:57 -0800

    fix: bug in post model

--- server/src/models/post.js
+++ server/src/models/post.js
@@ -8,7 +8,7 @@ const PostSchema = new Schema(
     likes: {
       type: [
         {
-          username: { type: String, required: true, unique: true },
+          username: { type: String, required: true },
         },
       ],
     },
